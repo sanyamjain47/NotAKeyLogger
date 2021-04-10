e129053c04841698201d5cc6906edbac4b46f476
Sanyam Ubuntu <sanyamjain47@gmail.com>
2021-04-10 23:55:12 +0530

    Code added for uploading data to ftp server

--- upload.py
+++ upload.py
@@ -1,10 +1,11 @@
 from ftplib import FTP
-from data import ftp_address,ftp_username,ftp_password, path
+from data import ftp_address,ftp_username,ftp_password
 import os
 
-filename = os.path.expanduser(path)
-ftp = FTP(ftp_address)
-ftp.login(ftp_username,ftp_password)
-with open(filename, "rb") as file:
-    ftp.storbinary("STOR test.log", file)
-ftp.quit()
+def upload_file(file_name):
+    filename = os.path.expanduser(file_name)
+    ftp = FTP(ftp_address)
+    ftp.login(ftp_username,ftp_password)
+    with open(filename, "rb") as file:
+        ftp.storbinary(f"STOR {filename.split('/')[-1]}", file)
+    ftp.quit()
